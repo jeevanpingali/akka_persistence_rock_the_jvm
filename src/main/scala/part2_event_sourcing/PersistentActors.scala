a846fa1e754c924c5d313fed3a1be8be0fa9f749
Jeevan Pingali <jeevan.pingali@gmail.com>
2021-01-24 16:26:46 +0530

    Before starting exercise

--- src/main/scala/part2_event_sourcing/PersistentActors.scala
+++ src/main/scala/part2_event_sourcing/PersistentActors.scala
@@ -9,6 +9,10 @@ object PersistentActors extends App {
 
   // COMMANDS
   case class Invoice(recipient: String, date: Date, amount: Int)
+  case class InvoiceBulk(invoices: List[Invoice])
+
+  // Special Messages
+  case object Shutdown
 
   // EVENTS
   case class InvoiceRecorded(id: Int, recipient: String, date: Date, amount: Int)
@@ -34,6 +38,26 @@ object PersistentActors extends App {
           log.info(s"Persisted $e as invoice${e.id}, for total amount $totalAmount")
         }
       }
+
+      case InvoiceBulk(invoices) => {
+        // create events
+        // persist all the events
+        // update the actor state when each event is persisted
+        val invoiceIds = latestInvoiceId to (latestInvoiceId + invoices.size)
+        val events = invoices.zip(invoiceIds).map{pair =>
+          val id = pair._2
+          val invoice = pair._1
+          InvoiceRecorded(id, invoice.recipient, invoice.date, invoice.amount)
+        }
+
+        persistAll(events) { e =>
+          latestInvoiceId += 1
+          totalAmount += e.amount
+          log.info(s"Persisted SINGLE $e as invoice${e.id}, for total amount $totalAmount")
+        }
+      }
+
+      case Shutdown => context.stop(self)
     }
 
     override def receiveRecover: Receive = {
@@ -41,6 +65,26 @@ object PersistentActors extends App {
         latestInvoiceId = id
         totalAmount += amount
         log.info(s"Recovered invoice #$id for amount $amount, total amount: $totalAmount")
+    }
+
+    /*
+      * This mehtod is called if persistence failed.
+      * The actor will be STOPPED
+      *
+      * Best practice, start the actor again, use Backoff supervisor
+      */
+    override def onPersistFailure(cause: Throwable, event: Any, seqNr: Long): Unit = {
+      log.error(s"Failed to persist event because of $cause")
+      super.onPersistFailure(cause, event, seqNr)
+    }
+
+    /*
+      Called if the JOURNAL fails persist the event
+      The actor is RESUMED
+     */
+    override def onPersistRejected(cause: Throwable, event: Any, seqNr: Long): Unit = {
+      log.error(s"Persistence rejected becasue of $cause")
+      super.onPersistRejected(cause, event, seqNr)
     }
   }
 
@@ -50,4 +94,27 @@ object PersistentActors extends App {
 /*  for(i <- 1 to 10) {
     accountant ! Invoice("The Sofa company", new Date(), i * 1000)
   }*/
+
+  /*
+  Persistence failures
+   */
+
+  /*
+  Persisting multiple events
+
+  persistAll
+   */
+
+  val newInvoices = for(i <- 1 to 5) yield Invoice ("The awesome chair", new Date(), i * 2000)
+//  accountant ! InvoiceBulk(newInvoices.toList)
+
+  /*
+    NEVER EVER CALL PERSIST OR PERSISTALL FROM FUTURES
+   */
+
+  /*
+    Shutdown of persist actors
+   */
+
+  accountant ! Shutdown
 }
---
+++ src/main/scala/part2_event_sourcing/PersistentActorsExercise.scala
@@ -0,0 +1,13 @@
+package part2_event_sourcing
+
+object PersistentActorsExercise extends App {
+  /**
+    * Implement persist actor for a voting station
+    * receives commands in the form of case class Vote (citizen, personal id, candidate)
+    * Keep citizens voted and the poll (mapping between of candidate and the number of received votes)
+    *
+    * the actor must be able to recover the state
+    */
+
+
+}
